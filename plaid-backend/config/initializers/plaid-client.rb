cfa83d3e4e58796564f3dc8979025b8345eb68ae
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-02 18:48:54 -0400

    created plaid controller, added initializer

--- plaid-backend/config/initializers/plaid-client.rb
+++ plaid-backend/config/initializers/plaid-client.rb
@@ -2,6 +2,10 @@ require 'plaid'
 
 
 $client = Plaid::Client.new(env: :sandbox,
-                            client_id: '***',
-                            secret: '***',
-                            public_key: '***')
+                            client_id: ENV["client_id"],
+                            secret: ENV["secret_key"],
+                            public_key: ENV["public_key"])
+
+response = item_response = $client.item.create(credentials: { username: 'user_good', password: 'pass_good' },
+											institution_id: 'ins_109509',
+											initial_products: %i(auth identity transactions))
