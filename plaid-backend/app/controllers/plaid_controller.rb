7e8383eb5adacefdcce36122c484b8e394538ecb
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-03 13:10:48 -0400

    using react-plaid-link because it works

--- plaid-backend/app/controllers/plaid_controller.rb
+++ plaid-backend/app/controllers/plaid_controller.rb
@@ -1,7 +1,11 @@
 class PlaidController < ApplicationController
 	def get_access_token
-		# store public token
-		# session[:public] = GET PARAMS HERE FROM REQUEST
-		# get access token from public token
+		# get access token from public token, no need to store public token, as it's a one-time use
+		# must store access token and item id
+		response = $client.item.public_token.exchange(params["public_token"])
+		session["access_token"] = response["access_token"]
+		session["item_id"] = response["item_id"]
+		byebug
 	end
+
 end
--- plaid-backend/config/routes.rb
+++ plaid-backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/get_access_token', to: 'plaid#get_access_token'
+  # match '/get_access_token', :controller => 'plaid', :action => 'options', :constraints => {:method => 'OPTIONS'}
+
 end
